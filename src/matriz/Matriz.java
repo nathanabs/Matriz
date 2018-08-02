54709267caf5328d2f899b108ffdc7ec38a0dc2e
nathan <nathanaguiarbs@gmail.com>
2018-08-02 17:15:58 -0300

    projeto criado

--- src/matriz/Matriz.java
+++ src/matriz/Matriz.java
@@ -5,6 +5,8 @@
  */
 package matriz;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Nathan
@@ -16,6 +18,44 @@ public class Matriz {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        Scanner sc = new Scanner(System.in);
+        int [][] matriz;
+        int tamColuna, tamLinhas, numero;
+        System.out.print("Digite a quantidade de linhas: ");
+        tamColuna = Integer.parseInt(sc.nextLine());
+        System.out.print("Digite a quantidade de colunas: ");
+        tamLinhas = Integer.parseInt(sc.nextLine());
+        
+        matriz = new int[tamColuna][tamLinhas];
+        
+        for (int i = 0; i < tamColuna; i++) {
+            for (int j = 0; j < tamLinhas; j++) {
+                matriz[i][j] = Integer.parseInt(sc.nextLine());
+            }
+        }
+        System.out.println("Digite um número da matriz: ");
+        numero = Integer.parseInt(sc.nextLine());
+        
+        for (int i = 0; i < tamColuna; i++) {
+            for (int j = 0; j < tamLinhas; j++) {
+                if(matriz[i][j]!= numero)
+                    continue;
+                System.out.println("Posicao: " +i+", "+j);
+                //Numero acima
+                if((i-1) >= 0)
+                    System.out.println("Acima: "+matriz[i-1][j]);
+                //Numero abaixo
+                if((i+1) < tamColuna)
+                    System.out.println("Abaixo: "+matriz[i+1][j]);
+                //Numero esquerda
+                if ((j-1) >= 0) 
+                    System.out.println("Esquerda: "+ matriz[i][j-1]);
+                //Numero direita
+                if((j+1) < tamLinhas)
+                    System.out.println("Direita: "+ matriz[i][j+1]);
+            }
+        }
     }
     
 }
